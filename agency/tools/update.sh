9734c4a39d380817c732de592de27b25a7dd87a8
Laura Vuorenoja <laura.vuorenoja@op.fi>
2020-09-23 09:53:39 +0300

    Fix application version update script.

--- agency/tools/update.sh
+++ agency/tools/update.sh
@@ -5,7 +5,7 @@ set -e
 CURRENT_DIR=$(dirname "$BASH_SOURCE")
 AWS_ACCOUNT_ID=$($CURRENT_DIR/aws-account-id.sh)
 
-VERSION=$(cat $CURRENT_DIR/../VERSION)
+VERSION=$(cat $CURRENT_DIR/../../VERSION)
 
 cd $CURRENT_DIR/../infra
 
@@ -16,6 +16,6 @@ make deploy-copy-s3-conf
 aws elasticbeanstalk create-application-version \
   --application-name findy-agency \
   --version-label $VERSION \
-  --source-bundle S3Bucket=findy-agency-beanstalk-configuration-$AWS_ACCOUNT_ID,S3Key=Dockerrun.zip
+  --source-bundle "S3Bucket=findy-agency-beanstalk-configuration-$AWS_ACCOUNT_ID,S3Key=Dockerrun.zip"
 
 make update-env UPDATE_APP_VERSION=$VERSION
