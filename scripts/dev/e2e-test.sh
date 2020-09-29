4d41d8c80988d788596772491b0772a316f62f65
Ville Seeste <ville.seeste@op.fi>
2020-09-29 19:49:44 +0300

    added tests

--- scripts/dev/e2e-test.sh
+++ scripts/dev/e2e-test.sh
@@ -25,6 +25,9 @@ e2e() {
   agency_env
   test_cmds
   stop_agency
+
+  other_cases
+  stop_agency
   rm_wallets
   echo -e "${BICYAN}*** E2E TEST FINISHED ***${NC}"
 }
@@ -67,12 +70,25 @@ rm_wallets() {
   rm ${CURRENT_DIR}/test_wallet1.export
   rm -rf ~/.indy_client/wallet/test_wallet1
   rm -rf ~/.indy_client/wallet/test_email1
+
+  rm -rf ~/.indy_client/wallet/user_test_wallet1
+  rm -rf ~/.indy_client/wallet/user_test_email1
+
+  rm -rf ~/.indy_client/wallet/user_test_wallet3
+  rm -rf ~/.indy_client/wallet/user_test_email3
+
+  rm -rf ~/.indy_client/wallet/user_test_wallet2
+  rm -rf ~/.indy_client/wallet/user_test_email2
+
   rm ${CURRENT_DIR}/test_wallet2.export
   rm -rf ~/.indy_client/wallet/test_wallet2
   rm -rf ~/.indy_client/wallet/test_email2
+
   rm ${CURRENT_DIR}/test_wallet3.export
   rm -rf ~/.indy_client/wallet/test_wallet3
   rm -rf ~/.indy_client/wallet/test_email3
+
+  rm ${CURRENT_DIR}/test_wallet4.export
   set -e
 }
 
@@ -117,9 +133,23 @@ set_envs() {
     export FCLI_ONBOARD_EXPORT_KEY="9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY"
     export FCLI_ONBOARD_SALT="my_test_salt"
 
+    export FCLI_FCLI_PING_SERVICE_ENDPOINT="true"
+
     export FCLI_SCHEMA_NAME="my_schema1"
     export FCLI_SCHEMA_VERSION="2.0"
     export FCLI_SCHEMA_ATTRIBUTES="[\"field1\", \"field2\", \"field3\"]"
+
+    export FCLI_CREDDEF_TAG="my_tag1"
+
+    export FCLI_KEY_SEED="000000000000000000000000Steward1"
+
+    export FCLI_USER_WALLET_NAME="user_test_wallet1"
+    export FCLI_USER_AGENCY_URL="http://localhost:8090"
+
+    export FCLI_INVITATION_LABEL="my_label"
+
+    export FCLI_SEND_MESSAGE="Hello!"
+    export FCLI_SEND_FROM="me"
 }
 
 cmds_env() {
@@ -133,9 +163,53 @@ cmds_env() {
   echo -e "${BLUE}*** env - onboard ***${NC}"
   $CLI service onboard
 
+  # ping
+  echo -e "${BLUE}*** env - ping ***${NC}"
+  $CLI service ping
+
   # create schema
   echo -e "${BLUE}*** env - create schema ***${NC}"
-  $CLI service schema create
+  sID=$($CLI service schema create)
+
+  # read schema
+  export FCLI_SCHEMA_ID=$sID
+  echo -e "${BLUE}*** env - read schema ***${NC}"
+  $CLI service schema read
+
+  # create creddef
+  export FCLI_CREDDEF_SCHEMA_ID=$sID
+  echo -e "${BLUE}*** env - create creddef ***${NC}"
+  cID=$($CLI service creddef create)
+
+  # read creddef
+  export FCLI_CREDDEF_ID=$cID
+  echo -e "${BLUE}*** env - read creddef ***${NC}"
+  $CLI service creddef read
+
+  # create key
+  echo -e "${BLUE}*** env - create key ***${NC}"
+  key=$($CLI tools key create)
+  echo $key
+
+  # user onboard
+  export FCLI_USER_WALLET_KEY=$key
+  unset FCLI_ONBOARD_EXPORT_FILE
+  echo -e "${BLUE}*** env - user onboard ***${NC}"
+  $CLI user onboard --email=user_test_email1
+
+  # invitation & connect
+  echo -e "${BLUE}*** env - invitation & connect ***${NC}"
+  conID=$($CLI user invitation | $CLI service connect - | sed 's#^.*}##'| sed -e 's#^.*\[\(.*\)\] ready#\1#' | tr -d '\n')
+
+  # trustping
+  echo -e "${BLUE}*** env - trustping ***${NC}"
+  export FCLI_TRUSTPING_CONNECTION_ID=$conID
+  $CLI service trustping
+
+  # send message
+  echo -e "${BLUE}*** env - send message ***${NC}"
+  export FCLI_SEND_CONNECTION_ID=$conID
+  $CLI service send
 }
 
 cmds_conf() {
@@ -151,10 +225,48 @@ cmds_conf() {
     --config=${CURRENT_DIR}/configs/onboard.yaml \
     --export-file=${CURRENT_DIR}/test_wallet2.export
 
+  # ping
+  echo -e "${BLUE}*** conf - ping ***${NC}"
+  $CLI service ping --config=${CURRENT_DIR}/configs/ping.yaml
+
   # create schema
   echo -e "${BLUE}*** conf - create schema ***${NC}"
-  $CLI service schema create \
-    --config=${CURRENT_DIR}/configs/createSchema.yaml
+  sID=$($CLI service schema create --config=${CURRENT_DIR}/configs/createSchema.yaml | sed 's#^.*yaml##' | tr -d '\n')
+
+  # read schema
+  echo -e "${BLUE}*** conf - read schema ***${NC}"
+  $CLI service schema read --config=${CURRENT_DIR}/configs/service.yaml --id=$sID
+
+  # create creddef
+  echo -e "${BLUE}*** conf - create creddef ***${NC}"
+  cID=$($CLI service creddef create --schema-id=$sID --config=${CURRENT_DIR}/configs/createCreddef.yaml | sed 's#^.*yaml##' | tr -d '\n')
+
+  # read creddef
+  echo -e "${BLUE}*** conf - read creddef ***${NC}"
+  $CLI service creddef read --id=$cID --config=${CURRENT_DIR}/configs/service.yaml
+
+  # create key
+  echo -e "${BLUE}*** conf - create key ***${NC}"
+  key=$($CLI tools key create --config=${CURRENT_DIR}/configs/key.yaml | sed 's#^.*yaml##' | tr -d '\n')
+  echo $key
+
+  # user onboard
+  echo -e "${BLUE}*** conf - user onboard ***${NC}"
+  $CLI user onboard --wallet-key=$key --config=${CURRENT_DIR}/configs/user.yaml
+
+  # invitation & connect
+  echo -e "${BLUE}*** conf - invitation & connect ***${NC}"
+  conID=$($CLI user invitation --wallet-key=$key --config=${CURRENT_DIR}/configs/user.yaml | sed 's#^.*yaml##' | tr -d '\n' |
+  $CLI service connect --config=${CURRENT_DIR}/configs/service.yaml - |
+  sed 's#^.*}##'| sed -e 's#^.*\[\(.*\)\] ready#\1#' | sed 's#^.*yaml##' | tr -d '\n')
+
+  # trustping
+  echo -e "${BLUE}*** conf - trustping ***${NC}"
+  $CLI service trustping --connection-id=$conID --config=${CURRENT_DIR}/configs/service.yaml
+
+  # send message
+  echo -e "${BLUE}*** conf - send message ***${NC}"
+  $CLI service send --connection-id=$conID --config=${CURRENT_DIR}/configs/service.yaml
 }
 
 cmds_flag() {
@@ -175,15 +287,81 @@ cmds_flag() {
     --email=test_email3 \
     --salt=my_test_salt
 
+  # ping
+  echo -e "${BLUE}*** flag - ping ***${NC}"
+  $CLI service ping \
+  --wallet-name=test_wallet3 \
+  --wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY \
+  --agency-url=http://localhost:8090
+
   # create schema
   echo -e "${BLUE}*** flag - create schema ***${NC}"
-  $CLI service schema create \
+  sID=$($CLI service schema create \
     --wallet-name=test_wallet3 \
     --agency-url=http://localhost:8090 \
     --wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY \
-    --name=my_schema3 \
-    --version="2.0" \
-    --attributes=["field1", "field2", "field3"]
+    --name=my_schema3 --version="2.0" --attributes="["field1", "field2", "field3"]")
+
+  # read schema
+  echo -e "${BLUE}*** flag - read schema ***${NC}"
+  $CLI service schema read \
+    --id=$sID \
+    --wallet-name=test_wallet3 \
+    --agency-url=http://localhost:8090 \
+    --wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY
+
+  # create creddef
+  echo -e "${BLUE}*** flag - create creddef ***${NC}"
+  cID=$($CLI service creddef create \
+    --wallet-name=test_wallet3 \
+    --agency-url=http://localhost:8090 \
+    --wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY \
+    --tag=my_tag2 \
+    --schema-id=$sID)
+
+  # read creddef
+  echo -e "${BLUE}*** flag - read creddef ***${NC}"
+  $CLI service creddef read \
+    --wallet-name=test_wallet3 \
+    --agency-url=http://localhost:8090 \
+    --wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY \
+    --id=$cID
+
+  # create key
+  echo -e "${BLUE}*** flag - create key ***${NC}"
+  key=$($CLI tools key create --seed=000000000000000000000000Steward1)
+  echo $key
+
+  # user onboard
+  echo -e "${BLUE}*** flag - user onboard ***${NC}"
+  $CLI user onboard \
+   --wallet-name=user_test_wallet3 \
+   --wallet-key=$key \
+   --agency-url=http://localhost:8090 \
+   --email=user_test_email3 \
+   --salt=my_test_salt
+
+  # invitation & connect
+  echo -e "${BLUE}*** flag - invitation & connect ***${NC}"
+  conID=$($CLI user invitation --label=my_invitation --wallet-name=user_test_wallet3 --wallet-key=$key --agency-url=http://localhost:8090 |
+    $CLI service connect --wallet-name=test_wallet3 --agency-url=http://localhost:8090 --wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY - |
+    sed 's#^.*}##'| sed -e 's#^.*\[\(.*\)\] ready#\1#' | tr -d '\n')
+
+  # trustping
+  echo -e "${BLUE}*** flag - trustping ***${NC}"
+  $CLI service trustping \
+   --wallet-name=test_wallet3 \
+   --agency-url=http://localhost:8090 \
+   --wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY \
+   --connection-id=$conID
+
+  # send message
+  echo -e "${BLUE}*** flag - send message ***${NC}"
+  $CLI service send \
+   --wallet-name=test_wallet3 \
+   --agency-url=http://localhost:8090 \
+   --wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY \
+   --connection-id=$conID --from=me --msg=Hello
 }
 
 agency_env() {
@@ -193,6 +371,8 @@ agency_env() {
   # launch and create pool
   echo -e "${BLUE}*** env - create pool ***${NC}"
   $CLI ledger pool create
+  echo -e "${BLUE}*** env - ping pool ***${NC}"
+  $CLI ledger pool ping
   echo -e "${BLUE}*** env - create steward ***${NC}"
   $CLI ledger steward create
 
@@ -212,6 +392,11 @@ agency_conf() {
   $CLI ledger pool create \
     --config=${CURRENT_DIR}/configs/createPool.yaml \
     --genesis-txn-file=${CURRENT_DIR}/genesis_transactions
+
+  echo -e "${BLUE}*** conf - ping pool ***${NC}"
+  $CLI ledger pool ping \
+    --config=${CURRENT_DIR}/configs/createPool.yaml
+
   echo -e "${BLUE}*** conf - create steward ***${NC}"
   $CLI ledger steward create \
     --config=${CURRENT_DIR}/configs/createSteward.yaml
@@ -232,6 +417,10 @@ agency_flag() {
   $CLI ledger pool create \
     --name=findy \
     --genesis-txn-file=${CURRENT_DIR}/genesis_transactions
+
+  echo -e "${BLUE}*** flag - ping pool ***${NC}"
+  $CLI ledger pool ping --name=findy
+
   echo -e "${BLUE}*** flag - create steward ***${NC}"
   $CLI ledger steward create \
     --pool-name=findy \
@@ -252,5 +441,79 @@ agency_flag() {
     --server-port=8090 \
     --salt=my_test_salt &
     sleep 2
+}
+
+other_cases() {
+  unset_envs
+  init_agency
+
+  # launch and create pool
+  echo -e "${BLUE}*** other - create pool ***${NC}"
+  $CLI ledger pool create \
+    --name=findy \
+    --genesis-txn-file=${CURRENT_DIR}/genesis_transactions
+
+  echo -e "${BLUE}*** other - ping pool ***${NC}"
+  $CLI ledger pool ping --name=findy
+
+  echo -e "${BLUE}*** other - import wallet ***${NC}"
+  $CLI tools import \
+    --key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY \
+    --file=${CURRENT_DIR}/steward.exported \
+    --wallet-name=steward \
+    --wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY
+
+  # run agency
+  echo -e "${BLUE}*** other - run agency ***${NC}"
+  docker start findy-pool
+  $CLI agency start \
+    --pool-name=findy \
+    --steward-wallet-name=steward \
+    --steward-wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY \
+    --steward-did=Th7MpTaRZVRYnPiabds81Y \
+    --steward-seed=000000000000000000000000Steward1 \
+    --host-port=8090 \
+    --server-port=8090 \
+    --salt=this is only example &
+    sleep 2
+
+  # onboard
+  echo -e "${BLUE}*** other - onboard ***${NC}"
+  $CLI service onboard \
+    --agency-url=http://localhost:8090 \
+    --wallet-name=test_wallet1 \
+    --wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY \
+    --email=test_email1 \
+    --salt=this is only example
+
+  # create schema
+  echo -e "${BLUE}*** other - create schema ***${NC}"
+  sID=$($CLI service schema create \
+    --wallet-name=test_wallet1 \
+    --agency-url=http://localhost:8090 \
+    --wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY \
+    --name=my_schema4 --version="2.0" --attributes="["field1", "field2", "field3"]")
+
+  # read schema
+  echo -e "${BLUE}*** other - read schema ***${NC}"
+  $CLI service schema read \
+    --id=$sID \
+    --wallet-name=test_wallet1 \
+    --agency-url=http://localhost:8090 \
+    --wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY
+
+  # export
+  echo -e "${BLUE}*** other - export wallet ***${NC}"
+  f=${CURRENT_DIR}/test_wallet4.export 
+  $CLI tools export \
+    --wallet-name=test_wallet1 \
+    --file=$f \
+    --key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY \
+    --wallet-key=9C5qFG3grXfU9LodHdMop7CNVb3HtKddjgRc7oK5KhWY
+  # check if file exist
+  if [ ! -f "$f" ]; then
+    echo "$f does not exist."
+    exit 1
+  fi
 }
 "$@"
