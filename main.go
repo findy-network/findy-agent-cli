92e7a1633e5ce438b6a0c632effff4550fad3d4f
Harri Lainio <harri.lainio@op.fi>
2021-03-14 10:27:44 +0200

    removing not used cmd packages

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"github.com/findy-network/findy-agent-cli/cmd"
-	_ "github.com/findy-network/findy-agent-cli/extracmd"
-	_ "github.com/findy-network/findy-agent-cli/extracmd/impl"
 	_ "github.com/findy-network/findy-agent-cli/extracmd/jwt"
 	_ "github.com/findy-network/findy-agent-cli/extracmd/jwt/authn"
 	_ "github.com/findy-network/findy-agent-cli/extracmd/jwt/bot"
 	_ "github.com/findy-network/findy-agent-cli/extracmd/ops"
-	_ "github.com/findy-network/findy-agent/grpc"
 )
 
 func main() {
