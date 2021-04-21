1d812982b61dcd4df05cfb349701009222bbbb9c
Harri Lainio <harri.lainio@op.fi>
2021-04-21 19:11:57 +0300

    new commands

---
+++ cmd/authn/add.go
@@ -0,0 +1,55 @@
+package authn
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/findy-network/findy-agent-cli/cmd"
+	"github.com/lainio/err2"
+	"github.com/spf13/cobra"
+)
+
+var addDoc = `Adds a new WebAuthn authenticator with given arguments to current account.
+The account is specified with --jwt argument. Note! That the authenticator
+commands are stateless which means that the caller must keep track all of the
+needed data. See more information from authn parent command.`
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Adds a new authenticator to JWT specified account",
+	Long:  addDoc,
+	PreRunE: func(c *cobra.Command, args []string) (err error) {
+		return cmd.BindEnvs(envs, "")
+	},
+	RunE: func(c *cobra.Command, args []string) (err error) {
+		defer err2.Return(&err)
+
+		myCmd := authnCmd
+		myCmd.SubCmd = "register" // register is the right command w/ --jwt
+
+		err2.Check(myCmd.Validate())
+		if !cmd.DryRun() {
+			c.SilenceUsage = true
+			r, err := myCmd.Exec(os.Stdout)
+			err2.Check(err)
+			fmt.Println(r.Token)
+		} else {
+			b, _ := json.MarshalIndent(myCmd, "", "  ")
+			fmt.Println(string(b))
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	defer err2.Catch(func(err error) {
+		fmt.Println(err)
+	})
+
+	addCmd.Flags().StringVar(&authnCmd.Token, "jwt", authnCmd.Token,
+		cmd.FlagInfo("Existing token to register a NEW authenticator", "", envs["jwt"]))
+	addCmd.MarkFlagRequired("jwt")
+	acatorCmd.AddCommand(addCmd)
+}
