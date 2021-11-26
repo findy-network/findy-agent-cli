e763191a6e441e6610feccf669e08c0e81ff5864
Harri Lainio <harri.lainio@op.fi>
2021-11-26 19:13:55 +0200

    add cfg.yaml and remove findy-root user from setup config

--- scripts/fullstack/setup-cli-env.sh
+++ scripts/fullstack/setup-cli-env.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 cli=${FCLI:-findy-agent-cli}
+. ../sa-compl.sh "$cli" "$cli"
 
 if [ -z "$FCLI_KEY" ]; then
 	export FCLI_KEY=`$cli new-key`
 	printf "export FCLI_KEY=%s" $FCLI_KEY > use-key.sh
 	echo "$FCLI_KEY" >> .key-backup
 fi
+export FCLI_CONFIG=./cfg.yaml
 export FCLI_TLS_PATH="$PWD/config/cert"
-export FCLI_USER="findy-root"
 export FCLI_URL=http://localhost:8088
 export FCLI_SERVER=localhost:50052
 export FCLI_ORIGIN=http://localhost:3000
 export FCLI=$cli
-
