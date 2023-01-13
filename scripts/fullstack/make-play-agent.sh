b242c2e0980e1b0b9e4aaa16852f10011bfb004a
Harri Lainio <harri.lainio@iki.fi>
2023-01-13 19:25:51 +0200

    adding new playground helper scripts

---
+++ scripts/fullstack/make-play-agent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+	echo 'usage: $0 <agent-name>'
+	exit 1
+fi
+
+agent_dir="./play/$1/"
+
+mkdir -p "$agent_dir"
+pushd "$agent_dir" > /dev/null
+ln -s ../../run/* .
+popd > /dev/null
+
+"$agent_dir"register && "$agent_dir"login
