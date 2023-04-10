5375f9a73e706bc5edee5110320bc0ca218a5666
Harri Lainio <harri.lainio@iki.fi>
2023-04-10 13:49:12 +0300

    symlink to absolute file

--- scripts/fullstack/setup-cli-env-local.sh
+++ scripts/fullstack/setup-cli-env-local.sh
@@ -1,1 +1,20 @@
-/home/parallels/host/go/src/github.com/findy-network/findy-agent-cli/scripts/fullstack/setup-cli-env-local.sh
+#!/bin/bash
+
+GOPATH=${GOPATH:-`go env GOPATH`}
+
+export FCLI=cli
+
+cli=${FCLI:-findy-agent-cli}
+. ../sa-compl.sh "$cli" "$cli"
+
+if [ -f ./use-key.sh ]; then
+	. ./use-key.sh
+fi
+
+if [ -z "$FCLI_KEY" ]; then
+	export FCLI_KEY=`$cli new-key`
+	printf "export FCLI_KEY=%s" $FCLI_KEY > use-key.sh
+	echo "$FCLI_KEY" >> .key-backup
+fi
+export FCLI_CONFIG=./cfg.yaml
+export FCLI_TLS_PATH="$GOPATH/src/github.com/findy-network/findy-agent/grpc/cert"
