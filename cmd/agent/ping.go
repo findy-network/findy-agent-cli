0c88e2cee51f50ba24884db39f0c4c096ec1332d
Harri Lainio <harri.lainio@iki.fi>
2024-01-21 17:41:40 +0200

    repeating, timing and progress bar added to ping

--- cmd/agent/ping.go
+++ cmd/agent/ping.go
@@ -3,10 +3,12 @@ package agent
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/findy-network/findy-agent-cli/cmd"
 	"github.com/findy-network/findy-common-go/agency/client"
 	agency "github.com/findy-network/findy-common-go/grpc/agency/v1"
+	"github.com/findy-network/findy-common-go/x"
 	"github.com/lainio/err2"
 	"github.com/lainio/err2/try"
 	"github.com/spf13/cobra"
@@ -30,24 +32,44 @@ var pingCmd = &cobra.Command{
 			fmt.Println("jwt:", CmdData.JWT)
 			return nil
 		}
+		var elapsedTotal time.Duration
+
 		baseCfg := try.To1(cmd.BaseCfg())
+		startTime := time.Now()
 		conn := client.TryAuthOpen(CmdData.JWT, baseCfg)
 		defer conn.Close()
 
 		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
 
+		info := "UNKNOWN"
 		agent := agency.NewAgentServiceClient(conn)
-		r := try.To1(agent.Enter(ctx, &agency.ModeCmd{
-			TypeID: agency.ModeCmd_NONE,
-		}))
-
-		fmt.Println("Agent registered by name:", r.Info)
+		elapsedTotal = time.Since(startTime)
+		fmt.Printf("Open conn time %v\n", elapsedTotal)
+		printProgressBar()
+		for i := 0; i < count; i++ {
+			startTime = time.Now()
+			r := try.To1(agent.Enter(ctx, &agency.ModeCmd{
+				TypeID: agency.ModeCmd_NONE,
+			}))
+			elapsed := time.Since(startTime)
+			elapsedTotal += elapsed
+			if i == 0 {
+				info = r.Info
+			}
+			printProgress(i)
+		}
+		fmt.Println("\nAgent registered by name:", info)
+		fmt.Printf("Meantime %v\n", elapsedTotal/time.Duration(count))
 		return nil
 	},
 }
 
-var andController bool
+var (
+	andController bool
+	count         int
+	preCount      int
+)
 
 func init() {
 	defer err2.Catch(err2.Err(func(err error) {
@@ -55,5 +77,28 @@ func init() {
 	}))
 	pingCmd.Flags().BoolVarP(&andController, "and-controller", "a", false,
 		"ping service agent as well")
+	pingCmd.Flags().IntVarP(&count, "count", "c", 1,
+		"how many times we should ping")
 	AgentCmd.AddCommand(pingCmd)
+}
+
+func printProgressBar() {
+	fmt.Println("pinging", count, "times")
+	countNormalized := x.Whom(count > 100, 100, count)
+	for i := 0; i < countNormalized; i++ {
+		fmt.Print(".")
+	}
+	fmt.Print("\r")
+}
+
+func printProgress(i int) {
+	if count > 100 {
+		normI := 100 * i / count
+		if normI != preCount || i == count-1 {
+			fmt.Print("o")
+			preCount = normI
+		}
+	} else {
+		fmt.Print("o")
+	}
 }
